652ec91826b42eddfc220e012da58e59192141fb
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-05-15 00:40:33 -0400

    fixing table

--- nodeWithMySQL.sql
+++ nodeWithMySQL.sql
@@ -1,10 +1,40 @@
 DROP DATABASE IF EXISTS bamazon_db;
 CREATE DATABASE bamazon_db;
 
+USE bamazon_db;
+
 CREATE TABLE products (
 item_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
 product_name VARCHAR(50),
 department_name VARCHAR(50),
 price INTEGER(10),
 stock_quantity VARCHAR(10)
-)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iPhone Case","Electronics",49.99,27);
+
+
+-- Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table).
+
+
+-- Then create a Node application called bamazonCustomer.js. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+
+
+-- The app should then prompt users with two messages.
+
+-- The first should ask them the ID of the product they would like to buy.
+-- The second message should ask how many units of the product they would like to buy.
+
+
+
+-- Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+-- If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+
+
+
+-- However, if your store does have enough of the product, you should fulfill the customer's order.
+
+-- This means updating the SQL database to reflect the remaining quantity.
+-- Once the update goes through, show the customer the total cost of their purchase.
